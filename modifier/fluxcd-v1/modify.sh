058dc99fc243b989cbeec09afbcdf7914ef72937
Dhi Aurrahman <dio@rockybars.com>
2023-09-04 17:53:36 +0700

    Update version
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- modifier/fluxcd-v1/modify.sh
+++ modifier/fluxcd-v1/modify.sh
@@ -8,6 +8,9 @@ set -e
 readonly ISTIO=$1
 readonly VERSION=$2
 
+export VERSION
+
+find ./charts/${ISTIO}/${VERSION} -name 'Chart.yaml'  -type f -exec yq -i '.version=env(VERSION)' {} \;
 sed -i '/\$whv := dict/,8d' charts/${ISTIO}/${VERSION}/istio-control/istio-discovery/templates/revision-tags.yaml
 sed -e '4i {{- $whv := dict "revision" .Values.revision "injectionPath" .Values.istiodRemote.injectionPath "injectionURL" .Values.istiodRemote.injectionURL "namespace" .Release.Namespace }}' -i charts/${ISTIO}/${VERSION}/istio-control/istio-discovery/templates/revision-tags.yaml
 sed -i '/\$whv := dict/,7d' charts/${ISTIO}/${VERSION}/istio-control/istio-discovery/templates/mutatingwebhook.yaml
