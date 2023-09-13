bfd2a79d4b0a667f2d347bc0784f4afe0583e4e7
Dhi Aurrahman <dio@rockybars.com>
2023-09-13 16:52:29 +0300

    Set agent version
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- magefiles/import_systems.go
+++ magefiles/import_systems.go
@@ -2,24 +2,58 @@ package main
 
 import (
 	"context"
+	"errors"
+	"os"
 	"path"
+	"strings"
 
 	"github.com/dio/magex/tool"
 )
 
 func importSystemAgent(ctx context.Context) error {
-	helmArgs := []string{
-		"package",
-		"-u",
-		"--destination", path.Join(dist(), "system"),
-	}
-	err := toolbox().RunWith(ctx, tool.RunWithOption{Env: map[string]string{
-		"TZ": "UTC",
-	}}, "helm", append(helmArgs, path.Join("charts", "system", "agent"))...)
+	base := path.Join("charts", "system")
+	entries, err := os.ReadDir(base)
 	if err != nil {
 		return err
 	}
 
+	for _, entry := range entries {
+		s := path.Join(base, entry.Name())
+		helmArgs := []string{
+			"package",
+			"-u",
+			"--destination", path.Join(dist(), "system"),
+		}
+
+		v, err := readVersion(s)
+		if err == nil {
+			helmArgs = append(helmArgs, "--app-version", strings.TrimPrefix(v, "v"), path.Join(s))
+		}
+
+		if err := toolbox().RunWith(ctx, tool.RunWithOption{Env: map[string]string{
+			"TZ": "UTC",
+		}}, "helm", helmArgs...); err != nil {
+			return err
+		}
+	}
+
 	// TODO(dio): Modify timestamp, using the last commit date.
 	return nil
+}
+
+func readVersion(name string) (string, error) {
+	entries, err := os.ReadDir(name)
+	if err != nil {
+		return "", err
+	}
+	for _, entry := range entries {
+		if entry.Name() == "VERSION" {
+			content, err := os.ReadFile(path.Join(name, entry.Name()))
+			if err != nil {
+				return "", err
+			}
+			return string(content), nil
+		}
+	}
+	return "", errors.New("failed to get version")
 }
